2411b2ad2c8abd862beabc79d4d03d2ff2f40bf2
Federico Cioschi <federicocioschi@gmail.com>
2015-05-24 21:40:42 +0200

    -------

--- archiver.rb
+++ archiver.rb
@@ -1,42 +1,44 @@
 
 require 'yomu'
+require 'mysql'
 
-puts "Type the folder path:"
-path = gets.chomp
-
-directory = Dir["#{path}/**/*.doc"]
-
-i=1
-
-directory.each do |f|
-	yomu = Yomu.new "#{f}"
-	text = yomu.text.gsub(/["]/, "'")
-	comment = yomu.metadata['Comments'].gsub(/["]/, "'")
+begin
+	#script-->user, password--> password of 'script' user, doc_db--> database name
+	con = Mysql.new 'localhost', 'script', 'password', 'doc_db'
 	
-	
-	require 'mysql'
+	check = con.query("SHOW TABLES LIKE \"doc_table\";")
+	if check.num_rows==0
+		con.query("create table doc_table(id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, file_name varchar(255), doc_text text, doc_comment text);")
+		puts "\"doc_table\" created\n"
+	end
 
-	begin
-		#script-->user, password--> password of 'script' user, ustica--> database name
-		con = Mysql.new 'localhost', 'script', 'password', 'ustica'
-		
-		input = "INSERT INTO ustica_doc (file_name, doc_text, doc_comment) VALUES(\"#{f}\", \"#{text}\", \"#{comment}\");"
+	puts "Type the folder path:"
+	path = gets.chomp
+
+	directory = Dir["#{path}/**/*.doc"]
+
+	i=1
+
+	directory.each do |f|
+		yomu = Yomu.new "#{f}"
+		text = yomu.text.gsub(/["]/, "'")
+		comment = yomu.metadata['Comments'].gsub(/["]/, "'")
+	
+		input = "INSERT INTO doc_table (file_name, doc_text, doc_comment) VALUES(\"#{f}\", \"#{text}\", \"#{comment}\");"
 		con.query(input)
 		
 		puts "#{i}) #{f} -->done"
 		i+=1
 	
-		rescue Mysql::Error => e
-			puts e.errno
-			puts e.error
-	    
-		ensure
-			con.close if con
 	end
 
+	rescue Mysql::Error => e
+		puts e.errno
+		puts e.error
+    
+	ensure
+		con.close if con
 end
-
-
 
 
 
