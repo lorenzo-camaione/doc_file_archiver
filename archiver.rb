6b0bd42509e3adc54fc8f0f41dd81e9bf5a435f5
Federico Cioschi <federicocioschi@gmail.com>
2015-06-05 17:26:01 +0200

    repetition controller added

--- archiver.rb
+++ archiver.rb
@@ -8,9 +8,9 @@ begin
 	#script-->user, password--> password of 'script' user, doc_db--> database name
 	con = Mysql.new 'localhost', 'script', 'password', 'doc_db'
 	
-	check = con.query("SHOW TABLES LIKE \"doc_table\";")
-	if check.num_rows==0
-		con.query("create table doc_table(id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, file_name varchar(255), doc_text text, doc_comment text);")
+	table_check = con.query("SHOW TABLES LIKE \"doc_table\";")
+	if table_check.num_rows==0
+		con.query("CREATE TABLE doc_table(id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, file_name varchar(255), doc_text text, doc_comment text, double_doc boolean);")
 		puts "\"doc_table\" created\n"
 	end
 
@@ -25,12 +25,24 @@ begin
 		yomu = Yomu.new "#{f}"
 		text = yomu.text.gsub('"', '\"')
 		comment = yomu.metadata['Comments'].gsub('"', '\"')
+		double = 0  #variable for the control of comments repetition
 		
+		comment_check = con.query("SELECT id FROM doc_table WHERE doc_comment=\"#{comment}\";")
+		if comment_check.num_rows!=0
+			double = 1
+		end
 		
-		input = "INSERT INTO doc_table (file_name, doc_text, doc_comment) VALUES(\"#{f}\", \"#{text}\", \"#{comment}\");"
+		input = "INSERT INTO doc_table (file_name, doc_text, doc_comment, double_doc) VALUES(\"#{f}\", \"#{text}\", \"#{comment}\", \"#{double}\");"
 		con.query(input)
 		
-		puts "#{i}) #{f} -->done"
+		print "#{i}) #{f} "
+		
+		if double == 1
+			puts "--> double"
+		else
+			puts "--> done"
+		end
+		
 		i+=1
 	
 	end
